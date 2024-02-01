4f9286c239f98e65377241b9fe7c1b8a26ecf59a
majorbruteforce <saswat3002@gmail.com>
2024-02-01 14:03:11 +0530

    harvest model

--- src/database/model/harvest.model.ts
+++ src/database/model/harvest.model.ts
@@ -1,25 +1,31 @@
 import mongoose, { Document, Schema } from 'mongoose';
+import ICrop from './crop.model';
+import { TQuantity } from '../types/quantity.type';
+
 
 interface IHarvest extends Document {
     _id?: mongoose.Types.ObjectId
-    name: string
-    variety: string
-    lifespan: number
+    quantity: number
+    crop: ICrop
+    rate: number
+    farmer: mongoose.Types.ObjectId
+    createdAt: Date
+    updatedAt: Date
+    producedAt: Date
+    bestUntil: number
 }
 
 const harvestSchema = new Schema<IHarvest>({
-    name: {
-        type: String,
-        required: true
-    },
-    variety : {
-        type: String,
-        required: true
-    },
-    lifespan: {
+    quantity: {
         type: Number,
         required: true
+    },
+    crop: {
+        type: Schema.Types.ObjectId,
+        ref: 'Crop',  // Reference to the Crop model
+        required: true,
     }
+
 });
 
 export const Crop = mongoose.model<IHarvest>('Harvest', harvestSchema);
---
+++ src/database/types/quantity.type.ts
@@ -0,0 +1,5 @@
+export type TQuantity= {
+    amount: number,
+    unit?: string
+}
+
