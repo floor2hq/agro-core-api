6daa252f526b705f943244b76cb2ce949ca1e0d1
zakhaev26 <b422056@iiit-bh.ac.in>
2024-02-01 22:53:13 +0530

    farm-crud-complete

--- src/routes/farm.route.ts
+++ src/routes/farm.route.ts
@@ -6,7 +6,6 @@ import mongoose from "mongoose";
 // import mongoose from "mongoose";
 const farmRouter = express.Router();
 
-
 interface customReq extends Request {
     user?: IUser
 }
@@ -37,14 +36,15 @@ farmRouter.get("/", authenticateToken, async (req: customReq, res: Response): Pr
     // @ts-ignore
     let ownerId = req.user?.user._id;
     ownerId = new mongoose.Types.ObjectId(ownerId)
-    console.log(ownerId);
+    // console.log(ownerId);
     try {
         const allFarms: IFarm[] | null = await Farm.find({
             owner: new mongoose.Types.ObjectId(ownerId)
         })
+            .populate('crops')
+            .populate('owner')
 
         res.status(200).json(allFarms);
-
     } catch (error: any) {
         console.log(error.message);
         res.status(400).json({
@@ -53,4 +53,33 @@ farmRouter.get("/", authenticateToken, async (req: customReq, res: Response): Pr
     }
 })
 
+farmRouter.patch("/", authenticateToken, async (req: customReq, res: Response): Promise<any> => {
+    const { _id, location, size, crops } = req.body;
+    // @ts-ignore
+    const owner = req.user?.user._id;
+    try {
+        const updatedFarm: any = await Farm.updateOne({ _id }, {
+            location,
+            size,
+            crops
+        })
+        res.status(200).send(updatedFarm);
+    } catch (error: any) {
+        console.log(error.message);
+        res.status(500).json({ error: error.message });
+    }
+})
+
+farmRouter.delete("/", authenticateToken, async (req: customReq, res: Response): Promise<any> => {
+    const { _id } = req.body;
+    // @ts-ignore
+    const owner = req.user?.user._id;
+    try {
+        const deletedFarm: any = await Farm.deleteOne({ _id })
+        res.status(200).send(deletedFarm);
+    } catch (error: any) {
+        console.log(error.message);
+        res.status(500).json({ error: error.message });
+    }
+})
 export default farmRouter;
